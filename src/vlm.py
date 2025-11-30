d13bf0fe28adeab3c9290575bdcc3b1df7dcb311
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2025-11-29 21:26:36 -0300

    Add the below functions: - vortexl: compute the induced velocity by vortex line
    - hshoe: compute a velocity induced by horseshoe vortex
    
    Change functions:
    meshPlanar: compute normals and area now
    
    plot: plot unit normals vector

--- src/vlm.py
+++ src/vlm.py
@@ -2,26 +2,30 @@ import numpy as np
 from scipy.linalg import solve
 import matplotlib.pyplot as plt
 
-def plot_mesh(X, Y, Z, vcp):
+def plot_mesh(X, Y, Z, vcp, normals):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    
+    scale = 0.05*np.max(X)
     for i in range(X.shape[0]):
         ax.plot(X[i,:], Y[i,:], Z[i,:], 'k-')
     for j in range(X.shape[1]):
         ax.plot(X[:,j], Y[:,j], Z[:,j], 'k-')
         
     for k in range(vcp.shape[0]):
-        # Plot center, quarter chord and three quarter chord points
-        ax.plot(vcp[k, 0, 0], vcp[k, 0, 1], vcp[k, 0, 2], 'ko', markersize = 4)
-        ax.plot(vcp[k, 1, 0], vcp[k, 1, 1], vcp[k, 1, 2], 'ro', markersize = 4)
-        # ax.plot(vcp[k, 2, 0], vcp[k, 2, 1], vcp[k, 2, 2], 'bo', markersize = 4)
-    
+        nx, ny, nz = normals[k]
+        cx, cy, cz = vcp[k, 2,:]
+        ax.plot(vcp[k, 0, 0], vcp[k, 0, 1], vcp[k, 0, 2], 'ko', markersize = 4) # center point
+        ax.plot(vcp[k, 1, 0], vcp[k, 1, 1], vcp[k, 1, 2], 'ro', markersize = 4) # quarter point
+        ax.plot(vcp[k, 2, 0], vcp[k, 2, 1], vcp[k, 2, 2], 'bo', markersize = 4) # three quarter point
+
+        ax.quiver(cx, cy, cz, nx, ny, nz, color = 'gray', length = scale)
+        
+        
     ax.set_xlabel("x")
     ax.set_ylabel("y")
-    ax.set_zlabel("z")
-    ax.set_box_aspect([1, 1, 0.1])
+    ax.set_zlabel("z") # type: ignore
+    ax.set_box_aspect([1, 1, 0.1]) # type: ignore
     plt.axis('equal')
     plt.show(block = False)
 
@@ -53,19 +57,34 @@ def meshPlanar(nspan:int, nchord:int, geometry:np.ndarray):
     n_panels = nchord * nspan
     panels_id = np.arange(n_panels).reshape(nchord, nspan)
     panels = np.zeros((n_panels, 4, 3))
-    k = 0
+    normals = np.zeros((n_panels, 3))
+    area = np.zeros((n_panels,1))
+    
     
     for j in range(nspan): 
         for i in range(nchord):  # percorre primeiro a corda depois o span
-            P1 = [X[i,   j],   Y[i,   j],   Z[i,   j]]
-            P2 = [X[i+1, j],   Y[i+1, j],   Z[i+1, j]]
-            P3 = [X[i+1, j+1], Y[i+1, j+1], Z[i+1, j+1]]
-            P4 = [X[i,   j+1], Y[i,   j+1], Z[i,   j+1]]
             
+            # Pontos do painel
+            P1 = np.array([X[i,   j],   Y[i,   j],   Z[i,   j]])
+            P2 = np.array([X[i+1, j],   Y[i+1, j],   Z[i+1, j]])
+            P3 = np.array([X[i+1, j+1], Y[i+1, j+1], Z[i+1, j+1]])
+            P4 = np.array([X[i,   j+1], Y[i,   j+1], Z[i,   j+1]])
+            
+            k = panels_id[i, j]
             panels[k,:,:] = np.array([P1, P2, P3, P4])
-            k += 1
+            
+            # Normais
+            u = P2 - P1
+            v = P4 - P1          
+            normal = np.cross(u, v)
+            area[k] = np.linalg.norm(normal)
+            normal_unit = normal/area[k]
+            normals[k, :] = normal_unit
+            
+            
+            
 
-    return X, Y, Z, panels
+    return X, Y, Z, panels, panels_id, normals, area
 
 def vortexAndControlPoint(panels:np.ndarray):
     
@@ -74,16 +93,145 @@ def vortexAndControlPoint(panels:np.ndarray):
     vcp = np.zeros((npanels, 3, 3)) # (panel x [center, quarter chord, three quarter chord])
     
     for k in range(npanels):
-        c_2 = np.mean(panels[k, :,:],axis=0)
-        c_4 = c_2.copy()
-        c_34 = c_2.copy()
+        center_point = np.mean(panels[k, :,:],axis=0)
+        quarter_point = center_point.copy()
+        three_quarter_point = center_point.copy()
         
-        c_4[0] = c_2[0] - c_2[0]/2
-        c_34[0] = c_2[0] + c_2[0]/2
+        half_chord = center_point[0] - panels[k,0,0] # final - inicial
         
-        vcp[k, 0, :] = c_2 
-        vcp[k, 1, :] = c_4
-        vcp[k, 2, :] = c_34 
+        quarter_point[0] = center_point[0] - half_chord/2
+        three_quarter_point[0] = center_point[0] + half_chord/2
+        vcp[k, 0, :] = center_point 
+        vcp[k, 1, :] = quarter_point
+        vcp[k, 2, :] = three_quarter_point 
     
     return vcp
+
+def vortexl(p:np.ndarray, p1:np.ndarray, p2:np.ndarray, Gamma:float = 1, tol:float = 1e-3) -> np.ndarray:
+    """_summary_
+
+    Parameters
+    ----------
+    p : np.ndarray
+        Point where the velocity will be compute
+    p1 : np.ndarray
+        Initial point of the vortex line
+    p2 : np.ndarray
+        Final point of the vortex line
+    Gamma : float, optional
+        Vorticity, by default 1
+    tol : float, optional
+        Tolerance to avoid singularities, by default 1e-3
+
+    Returns
+    -------
+    np.ndarray
+        Induced velocity
+    """
+    # Distances vectors
+    r1 = p - p1
+    r2 = p - p2
+    r0 = p
     
+    # Unit vectos
+    r1_norm = np.linalg.norm(r1)
+    r2_norm = np.linalg.norm(r2)
+    
+    assert r1_norm > tol, "vortexl: Singular condition, r1 is close enough of the core"
+    assert r2_norm > tol, "vortexl: Singular condition, r2 is close enough of the core"
+    
+    r1_unit = r1/r1_norm
+    r2_unit = r2/r2_norm
+    
+    # Cross product
+    r1xr2 = np.cross(r1, r2)
+    r1xr2_norm_square = np.linalg.norm(r1xr2)**2
+    
+    assert r1xr2_norm_square > tol, "vortexl: Singular condition, |r1xr2|^2 is close enought of the core"  
+    
+    # velocity
+    K = Gamma/(4*np.pi)/r1xr2_norm_square *(r0 @ r1_unit - r0 @ r2_unit)
+    q12 = K*r1xr2
+    
+    return q12
+    
+def hshoe(p:np.ndarray, pa:np.ndarray, pb:np.ndarray, pc:np.ndarray, pd:np.ndarray, Gamma:float = 1, tol:float = 1e-3)-> tuple:
+    """_summary_
+
+    Parameters
+    ----------
+    p : np.ndarray
+        _description_
+    pa : np.ndarray
+        _description_
+    pb : np.ndarray
+        _description_
+    pc : np.ndarray
+        _description_
+    pd : np.ndarray
+        _description_
+    Gamma : float, optional
+        _description_, by default 1
+    tol : float, optional
+        _description_, by default 1e-3
+
+    Returns
+    -------
+    tuple
+        _description_
+    """
+    iv1 = vortexl(p, pa, pb, Gamma = Gamma, tol=tol)
+    iv2 = vortexl(p, pb, pc, Gamma = Gamma, tol=tol)
+    iv3 = vortexl(p, pc, pd, Gamma = Gamma, tol=tol)
+    
+    aij = iv1+iv2+iv3
+    bij = iv1+iv3       # induced velocity by wake
+    
+    return aij, bij
+
+def influence_coefficients(Vinf:np.ndarray,aoa:float, l_inf:float, panels:np.ndarray, vcp:np.ndarray, normals:np.ndarray)->tuple:
+    """_summary_
+
+    Parameters
+    ----------
+    Vinf : np.ndarray
+        _description_
+    aoa : float
+        _description_
+    l_inf : float
+        _description_
+    panels : np.ndarray
+        _description_
+    vcp : np.ndarray
+        _description_
+    normals : np.ndarray
+        _description_
+
+    Returns
+    -------
+    tuple
+        _description_
+    """
+    
+    npanels = panels.shape[0]
+    
+    a = np.zeros((npanels, npanels))
+    b = np.zeros((npanels, npanels))
+    RHS = np.zeros(npanels)
+    for ki in range(npanels):
+        p = vcp[ki, 2, :]
+        RHS[ki] = -Vinf*np.sin(aoa) @ normals[ki]
+        for kj in range(npanels):
+            span = panels[kj, 3, 1] - panels[kj, 0, 1]
+            
+            # Defini points of the horseshoe vortex
+            pa = vcp[kj, 1, :] + np.array([l_inf, -span/2, 0]) # point at infinite
+            pb = vcp[kj, 1, :] + np.array([0, -span/2, 0])
+            pc = vcp[kj, 1, :] + np.array([0, +span/2, 0])
+            pd = vcp[kj, 1, :] + np.array([l_inf, +span/2, 0]) # point at infinite
+            
+            aij, bij = hshoe(p, pa, pb, pc, pd)
+            a[ki, kj] = aij @ normals[ki]
+            b[ki, kj] = bij @ normals[ki]
+    
+    return a, b, RHS
