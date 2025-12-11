e9842e177aa2c136c67a2cbfd1086b5507fc94a3
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2025-12-10 22:56:41 -0300

    change for loop to get polar

--- flat_plate.py
+++ flat_plate.py
@@ -1,5 +1,23 @@
 import numpy as np
 import src.vlm as vlm
+import matplotlib.pyplot as plt
+
+"""
+Atividades VLM com vórtice de ferradura: 
+    - Alihar esteira com velocidade freestream
+    - Conferir span multiplicando sustentação
+    - Calcular arrasto
+    - Condição de simetria
+    - Testar diedro
+    - Enflechamento
+    - Arqueamento
+    
+Atividade VLM com Kawada-Hardin:
+    - Implementar velocidade induzida pela esteira helicoidal
+    - Adaptar condições de operação para rotor
+"""
+
+
 #%% Geometria da asa
 span = 5
 y = np.linspace(-span/2, span/2, 5)
@@ -8,13 +26,17 @@ chord = np.ones_like(y)
 
 geometry = np.column_stack([x_le, y, chord])
 
-nspan  = 10
+nspan  = 20
 nchord = 5
 
 mesh = vlm.meshPlanar(nspan, nchord, geometry)
 vlm.plot_mesh(mesh)
 #%% Cl xalpha
-alphas = np.deg2rad(np.arange(0, 6))
+alphas_deg = np.arange(0, 6)
+alphas = np.deg2rad(alphas_deg)
+
+# alphas = np.deg2rad([3])
+
 Cl = np.zeros_like(alphas)
 rho = 1.22
 Vtot = 10
@@ -22,7 +44,7 @@ q = 0.5*rho*Vtot**2
 S = span*np.mean(chord)
 c = np.mean(chord)
 for i, aoa in enumerate(alphas):
-    Vinf = Vtot*np.array([np.cos(aoa), np.sin(aoa), 0])
+    Vinf = Vtot*np.array([np.cos(aoa), 0, np.sin(aoa)])
     # Resolver sistemas
     A,B,RHS = vlm.influence_coefficients(Vinf, 500, mesh)
     Gamma = np.linalg.solve(A, RHS)
@@ -30,12 +52,31 @@ for i, aoa in enumerate(alphas):
     #% Kutta-Joukwski theorem
     
     panels_span =mesh['panels_span'].reshape(Gamma.shape)
-    print(f'{Vtot=}')
     Li = rho*Vtot*Gamma*panels_span
     L = np.sum(Li)
     
     Cl[i]  = L/(q*S*c)
+    print(f'alpha = {alphas_deg[i]:.1f}: Cl={Cl[i]:.5f}')
 
+#%% dados xflr5
+xf = np.loadtxt('data/retangular-10_0 m_s-case1.txt', skiprows=8, usecols=(0, 2, 3))
+
+plt.figure()
+plt.subplot(1,2,1)
+plt.plot(xf[:, 0], xf[:, 1], 'bo-')
+plt.plot(alphas_deg, Cl, 'ks-')
+plt.xlabel(r'$\alpha$ [deg]')
+plt.ylabel(r'$C_L$')
+plt.grid()
+
+plt.subplot(1,2,2)
+plt.plot(xf[:, 0], xf[:, 2], 'ro-')
+plt.xlabel(r'$\alpha$ [deg]')
+plt.ylabel(r'$C_{Di}$')
+plt.grid()
+
+plt.tight_layout()
+plt.show()
 # %% asa enflechada
 # span = 4.0
 # c_root = 1
