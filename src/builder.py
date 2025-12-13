e84b2bba753d14d92c88e9260ad5c951fce5e0d5
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2025-12-13 18:00:56 -0300

    add configuration files to run avl using avlwrapper

---
+++ src/builder.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from configparser import ConfigParser
+from avlwrapper import Configuration
+
+################################################################
+# AVL and Project Path
+################################################################
+
+avlPath = Path('bin','avl352.exe').absolute()
+if not avlPath.exists():
+    avlPath = Path('..', 'bin', 'avl352.exe').absolute()
+
+PROJECT = avlPath.parent.parent
+
+cfg_path = PROJECT.joinpath('config.cfg')
+
+################################################################
+# Setup to run avl
+################################################################
+def config_file(PATH_AVL):
+    avl_path = PATH_AVL
+
+    config = ConfigParser()
+
+    config['environment'] = {
+        'Executable': avl_path.absolute(),
+        'PrintOutput': 'no',
+        'GhostscriptExecutable': 'gs',
+    }
+
+    config['output'] = {
+        'Totals': 'yes',
+        'SurfaceForces': 'yes',
+        'StripForces': 'yes',
+        'ElementForces': 'yes',
+        'BodyAxisDerivatives': 'yes',
+        'StabilityDerivatives': 'yes',
+        'HingeMoments': 'yes',
+        'StripShearMoments': 'yes',
+    }
+
+    with open(cfg_path, 'w') as config_file:
+        config.write(config_file)
+
+config_file(avlPath)
+my_config = Configuration(cfg_path)
