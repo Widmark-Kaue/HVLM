5d2a6770e61792e7ad218f947afd9d491cf9d73b
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2025-12-16 14:24:09 -0300

    tests with diferent geometrys

--- validation.py
+++ validation.py
@@ -1,7 +1,7 @@
 #%%
 import numpy as np
 import src.vlm as vlm
-from src.utils import set_aiaa_style
+from src.utils import set_aiaa_style, plot_polar
 import matplotlib.pyplot as plt
 from pathlib import Path
 
@@ -10,18 +10,19 @@ from matplotlib import use
 set_aiaa_style()
 """
 Atividades VLM com vórtice de ferradura: 
-    - Conferir span multiplicando sustentação
+    - Conferir span multiplicando sustentação [parcial check]
     - Calcular arrasto [parcial check]
     - Condição de simetria [check]
-    - Testar diedro
-    - Enflechamento
+    - Testar diedro [parcial check]
+    - Enflechamento [parcial check]
     - Arqueamento
     
 Atividade VLM com Kawada-Hardin:
     - Implementar velocidade induzida pela esteira helicoidal
     - Adaptar condições de operação para rotor
 """
-#%%
+#%% functions
+
 
 #%%
 # =============================================================================
@@ -46,49 +47,36 @@ case = 'rectangular_wing'
 geo_rec = vlm.geometry(span, AR)
 mesh_rec = vlm.meshPlanar(nspan, nchord, geo_rec)
 vlm.plot_mesh(mesh_rec, 'Rectangular Wing')
-CL, CDi, CDi2 = vlm.run_polar(Vinf=Vinf, rho=rho,alpha=alpha, mesh=mesh_rec)
-avlData = np.loadtxt(path/case/'polar.dat')
-
-plt.figure(figsize=(8, 3.5))
-plt.subplot(1, 2, 1)
-plt.plot(avlData[:,0], avlData[:,1], 'k-o', label = 'AVL')
-plt.plot(alpha, CL, 'r--s', label = 'code')
-# plt.yticks(avlData[:,1])
-plt.xlabel(r'$\alpha$ [deg]')
-plt.ylabel(r'$C_{L}$')
-# plt.grid()
-plt.legend()
-
-plt.subplot(1, 2, 2)
-plt.plot(avlData[:,0], avlData[:,2], 'k-o', label = 'AVL')
-plt.plot(alpha, CDi, 'r--s', label = 'code')
-plt.plot(alpha, CDi2, 'm--^', label = 'code 2')
-# plt.yticks(avlData[:,1])
-plt.xlabel(r'$\alpha$ [deg]')
-plt.ylabel(r'$C_{D_i}$')
-# plt.legend()
-
-plt.tight_layout()
-plt.show(block = False)
-
-
+vlmData = vlm.run_polar(Vinf=Vinf, alpha=alpha, mesh=mesh_rec)
+plot_polar(vlmData=vlmData, path_case=path.joinpath(case, 'polar.dat'), savefig=True)
 
 #%% Wing with taper ratio
+plt.close('all')
+case = 'taper_wing'
 geo_taper = vlm.geometry(span, AR, taper=0.6)
 mesh_taper = vlm.meshPlanar(nspan, nchord, geo_taper)
 vlm.plot_mesh(mesh_taper, r'Wing with taper ratio: $\lambda = 0.6$')
+vlmData = vlm.run_polar(Vinf=Vinf, alpha=alpha, mesh =mesh_taper)
+plot_polar(vlmData=vlmData, path_case=path.joinpath(case, 'polar.dat'), savefig=True)
 
-# Wing with sweep
+#%% Wing with sweep
+plt.close('all')
+case = 'sweep_wing'
 geo_sweep = vlm.geometry(span, AR, sweep=10)
 mesh_sweep= vlm.meshPlanar(nspan, nchord, geo_sweep)
 vlm.plot_mesh(mesh_sweep, r'Wing with sweep: $\phi = 10^\circ$')
+vlmData = vlm.run_polar(Vinf=Vinf, alpha=alpha, mesh=mesh_sweep)
+plot_polar(vlmData=vlmData, path_case=path.joinpath(case, 'polar.dat'), savefig=True)
 
-# Wing with twist
+
+#%% Wing with twist
+plt.close('all')
+case = 'twist_wing'
 geo_twist = vlm.geometry(span, AR, twist=5)
 mesh_twist = vlm.meshPlanar(nspan, nchord, geo_twist)
 vlm.plot_mesh(mesh_twist, r'Wing with twist: $\theta = 5^\circ$')
-
-plt.close('all')
-
+vlmData = vlm.run_polar(Vinf=Vinf, alpha=alpha, mesh=mesh_twist)
+plot_polar(vlmData=vlmData, path_case=path.joinpath(case, 'polar.dat'), savefig=True)
+pass
 
     
