013d3415b38fd62babbd9d3d68109d87d1561824
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2025-12-17 16:38:31 -0300

    using plot_polar

--- validation.py
+++ validation.py
@@ -33,7 +33,7 @@ Vinf = 1
 rho = 1.225
 alpha = np.arange(0, 12, 1)
 path = Path('data', 'avlCases')
-
+savefig = False
 # Geometric parameters
 span = 1
 AR = 8 
@@ -48,7 +48,7 @@ geo_rec = vlm.geometry(span, AR)
 mesh_rec = vlm.meshPlanar(nspan, nchord, geo_rec)
 vlm.plot_mesh(mesh_rec, 'Rectangular Wing')
 vlmData = vlm.run_polar(Vinf=Vinf, alpha=alpha, mesh=mesh_rec)
-plot_polar(vlmData=vlmData, path_case=path.joinpath(case, 'polar.dat'), savefig=True)
+plot_polar(vlmData=vlmData, path_case=path.joinpath(case, 'polar.dat'), savefig=savefig)
 
 #%% Wing with taper ratio
 plt.close('all')
@@ -57,7 +57,7 @@ geo_taper = vlm.geometry(span, AR, taper=0.6)
 mesh_taper = vlm.meshPlanar(nspan, nchord, geo_taper)
 vlm.plot_mesh(mesh_taper, r'Wing with taper ratio: $\lambda = 0.6$')
 vlmData = vlm.run_polar(Vinf=Vinf, alpha=alpha, mesh =mesh_taper)
-plot_polar(vlmData=vlmData, path_case=path.joinpath(case, 'polar.dat'), savefig=True)
+plot_polar(vlmData=vlmData, path_case=path.joinpath(case, 'polar.dat'), savefig=savefig)
 
 #%% Wing with sweep
 plt.close('all')
@@ -66,7 +66,7 @@ geo_sweep = vlm.geometry(span, AR, sweep=10)
 mesh_sweep= vlm.meshPlanar(nspan, nchord, geo_sweep)
 vlm.plot_mesh(mesh_sweep, r'Wing with sweep: $\phi = 10^\circ$')
 vlmData = vlm.run_polar(Vinf=Vinf, alpha=alpha, mesh=mesh_sweep)
-plot_polar(vlmData=vlmData, path_case=path.joinpath(case, 'polar.dat'), savefig=True)
+plot_polar(vlmData=vlmData, path_case=path.joinpath(case, 'polar.dat'), savefig=savefig)
 
 
 #%% Wing with twist
@@ -76,7 +76,7 @@ geo_twist = vlm.geometry(span, AR, twist=5)
 mesh_twist = vlm.meshPlanar(nspan, nchord, geo_twist)
 vlm.plot_mesh(mesh_twist, r'Wing with twist: $\theta = 5^\circ$')
 vlmData = vlm.run_polar(Vinf=Vinf, alpha=alpha, mesh=mesh_twist)
-plot_polar(vlmData=vlmData, path_case=path.joinpath(case, 'polar.dat'), savefig=True)
+plot_polar(vlmData=vlmData, path_case=path.joinpath(case, 'polar.dat'), savefig=savefig)
 pass
 
     
