54618dd6b7c11ede705ebccc515ee94d442fc68c
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2025-12-22 19:09:42 -0300

    test of camber implementation

--- validation2.py
+++ validation2.py
@@ -14,13 +14,13 @@ AR = 8
 taper = 0.6
 sweep = 10
 twist = 5
-
+sections = ['Naca 0012', 'Naca 0012']
 # Mesh parameters
 nspan  = 5
 nchord = 3
 
 #%% Rectangular Wing
-wing = Wing(span=span, AR=AR, name = 'rectangular_wing')
+wing = Wing(span=span, AR=AR, sections=sections,name = 'rectangular_wing')
 wing.mesh(nspan=nspan, nchord=nchord)
 wing.plot_mesh(True)
 wing_sim = VLM(wing)
@@ -29,10 +29,9 @@ plot_polar(polar, path_case=path.joinpath(wing.name, 'polar.dat'),
            savefig=savefig)
 
 
-
 #%% Wing with taper ratio
 plt.close('all')
-wing = Wing(span=span, AR = AR, taper=taper, name='taper_wing')
+wing = Wing(span=span, AR = AR, sections=sections, taper=taper, name='taper_wing')
 wing.mesh(nspan=nspan, nchord=nchord)
 wing.plot_mesh(title=True)
 polar = wing_sim.run_polar(Vinf=Vinf, alpha=alpha, ref='drela')
@@ -43,7 +42,7 @@ plot_polar(polar, path_case=path.joinpath(wing.name, 'polar.dat'),
 
 #%% Wing with sweep
 plt.close('all')
-wing = Wing(span=span, AR = AR, sweep=sweep, name = 'sweep_wing')
+wing = Wing(span=span, AR = AR, sections=sections, sweep=sweep, name = 'sweep_wing')
 wing.mesh(nspan=nspan, nchord=nchord)
 wing.plot_mesh(title=True)
 polar = wing_sim.run_polar(Vinf=Vinf, alpha=alpha)
@@ -53,10 +52,20 @@ plot_polar(polar, path_case=path.joinpath(wing.name, 'polar.dat'),
 
 #%% Wing with twist
 plt.close('all')
-wing = Wing(span=span, AR = AR, twist=twist, name = 'twist_wing')
+wing = Wing(span=span, AR = AR, sections=sections, twist=twist, name = 'twist_wing')
 wing.mesh(nspan=nspan, nchord=nchord)
 wing.plot_mesh(title=True)
 plot_polar(polar, path_case=path.joinpath(wing.name, 'polar.dat'), 
            savefig=savefig)
 
+#%% Wing with camber
+plt.close('all')
+sections = ['Naca 2412', 'Naca 2412']
+wing = Wing(span=span, AR=AR, sections=sections,name = 'camber_wing')
+wing.mesh(nspan=nspan, nchord=nchord)
+wing.plot_mesh(True)
+wing_sim = VLM(wing)
+polar = wing_sim.run_polar(Vinf=Vinf, alpha=alpha, ref='drela')
+plot_polar(polar, path_case=path.joinpath(wing.name, 'polar.dat'), 
+           savefig=savefig)
 pass
